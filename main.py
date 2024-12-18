4551a5074afda61cedd019df6d3609d6d4f7bb7e
Toni Dezman <toni.dezman@gmail.com>
2024-12-18 10:58:33 +0100

    errors are fixed

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window
 
-class Window():
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.root = Tk()
-        self.title = self.root.title("Window")
-        self.canvas = Canvas(self.root, width=self.width, height=self.height)
-        self.pack = self.canvas.pack(fill=BOTH, expand=1)
-        self.windows_is_running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-
-    def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
-        self.windows_is_running = True
-        while self.windows_is_running:
-            self.redraw()
-
-    def wait_for_close(self):
-        self.windows_is_running = True
-        while self.windows_is_running:
-            self.redraw()
-
-    def close(self):
-        self.windows_is_running = False
 
 def main():
-    window = Window(800, 600)
-    window.wait_for_close()
+    win = Window(800, 600)
+    win.wait_for_close()
+
 
 main()
