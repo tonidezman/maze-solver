50a6e5d14b6ceda92b0a03eef0d29973c39a7e8a
Toni Dezman <toni.dezman@gmail.com>
2024-12-18 11:21:10 +0100

    moving classes to diff file

--- graphics.py
+++ graphics.py
@@ -1,5 +1,21 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+
+class Line:
+    def __init__(self, p1: Point, p2: Point):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas: Canvas, fillcolor:str, width:int):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fillcolor, width=width)
 
 class Window:
     def __init__(self, width, height):
@@ -25,3 +41,6 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line: Line, fillcolor: str, width: int):
+        line.draw(self.__canvas, fillcolor, width)
--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-from tkinter import Canvas
-from graphics import Window
+from graphics import Window, Line, Point
 
-class Point:
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-
-
-class Line:
-    def __init__(self, p1: Point, p2: Point):
-        self.p1 = p1
-        self.p2 = p2
-
-    def draw(self, canvas: Canvas):
-        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill="black", width=2)
 
 def main():
     win = Window(800, 600)
@@ -24,7 +7,7 @@ def main():
     p1 = Point(100, 100)
     p2 = Point(200, 200)
     line = Line(p1, p2)
-    line.draw(win.get_canvas())
+    win.draw_line(line, fillcolor="red", width=2)
 
     win.wait_for_close()
 
